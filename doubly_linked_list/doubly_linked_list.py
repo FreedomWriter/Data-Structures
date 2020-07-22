dfd9c6411b61365f40533afc3860529269092ac8
FreedomWriter <natalienetworks@gmail.com>
2020-07-22 13:30:36 -0500

    singly linked list tests passing

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -140,4 +140,12 @@ class DoublyLinkedList:
     in the List.
     """
     def get_max(self):
-        pass
+        if not self.head:
+            return None
+        max_value = self.head.value
+        current = self.head.next
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.next
+        return max_value
